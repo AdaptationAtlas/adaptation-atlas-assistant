240cd861b1b76a8e7e798300a35675da0295d145
aliziel <21992503+aliziel@users.noreply.github.com>
2025-12-15 18:44:19 -0800

    fix: use more precision in naming reference to application-specific variables

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -12,7 +12,7 @@ locals {
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   container_port = 8000
-  env = {
+  application_env = {
     MISTRAL_API_KEY      = var.mistral_api_key
     CHAT_MODEL_SIZE      = var.chat_model_size
     JWT_KEY              = var.jwt_key
