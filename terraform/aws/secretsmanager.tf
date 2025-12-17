6c2e9fabd17427e454559bff28f51a596bf90b25
Pete Gadomski <pete.gadomski@gmail.com>
2025-12-17 13:02:59 -0700

    fix: bump the secret version

--- terraform/aws/secretsmanager.tf
+++ terraform/aws/secretsmanager.tf
@@ -5,5 +5,5 @@ resource "aws_secretsmanager_secret" "backend_secrets" {
 resource "aws_secretsmanager_secret_version" "backend_secrets" {
   secret_id                = aws_secretsmanager_secret.backend_secrets.id
   secret_string_wo         = jsonencode(local.env)
-  secret_string_wo_version = 6
+  secret_string_wo_version = 7
 }
